e71c9d4891d1f11da14e6970e1cb4d57f6546a81
Kristoffer <kcarlsson89@gmail.com>
2024-11-21 09:49:30 +0100

    run with more cores on apple

--- .ci/create_sysimage_and_run_tests.jl
+++ .ci/create_sysimage_and_run_tests.jl
@@ -4,4 +4,6 @@ sysimage = tempname()
 create_sysimage(["LinearAlgebra", "Test", "Distributed", "Dates", "REPL", "Printf", "Random"]; sysimage_path=sysimage, incremental=false, filter_stdlibs=true)
 
 current_dir = @__DIR__
-run(`$(Base.julia_cmd()) --sysimage=$sysimage --project=$current_dir -e 'Base.runtests(["LinearAlgebra"]; propagate_project=true)'`)
+ncores = Sys.isapple() ? Sys.CPU_THREADS : ceil(Int, Sys.CPU_THREADS / 2)
+cmd = """Base.runtests(["LinearAlgebra"]; propagate_project=true, ncores=$ncores)"""
+run(`$(Base.julia_cmd()) --sysimage=$sysimage --project=$current_dir -e $cmd`)

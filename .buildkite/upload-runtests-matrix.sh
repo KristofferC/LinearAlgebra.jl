3280bc68e8e6c7063a603afacb05a4a19e50204a
Fredrik Ekre <ekrefredrik@gmail.com>
2024-11-21 10:39:42 +0100

    rm unused files

--- .buildkite/upload-runtests-matrix.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while read -r OS ARCH ROOTFS_IMAGE_NAME ROOTFS_TAG ROOTFS_ARCH ROOTFS_HASH; do
-    # Skip empty lines and comments
-    [[ -z "${OS}" || "${OS}" == \#* ]] && continue
-      export OS ARCH ROOTFS_IMAGE_NAME ROOTFS_TAG ROOTFS_ARCH ROOTFS_HASH
-      buildkite-agent pipeline upload ./.buildkite/runtests.yml
-done < ".buildkite/runtests-matrix.txt"

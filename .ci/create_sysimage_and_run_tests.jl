88f083a718b983ef5b6a05389e8cc9cd15694547
Kristoffer <kcarlsson89@gmail.com>
2024-11-21 10:47:45 +0100

    revert thingy

--- .ci/create_sysimage_and_run_tests.jl
+++ .ci/create_sysimage_and_run_tests.jl
@@ -1,9 +1,15 @@
 using PackageCompiler
 
 sysimage = tempname()
-create_sysimage(["LinearAlgebra", "Test", "Distributed", "Dates", "REPL", "Printf", "Random"]; sysimage_path=sysimage, incremental=false, filter_stdlibs=true)
+
+ncores = Sys.isapple() ? Sys.CPU_THREADS : ceil(Int, Sys.CPU_THREADS / 2)
+
+withenv("JULIA_IMAGE_THREADS" => ncores) do
+    create_sysimage(["LinearAlgebra", "Test", "Distributed", "Dates", "REPL", "Printf", "Random"]; sysimage_path=sysimage, incremental=false, filter_stdlibs=true)
+end
 
 current_dir = @__DIR__
-ncores = Sys.isapple() ? Sys.CPU_THREADS : ceil(Int, Sys.CPU_THREADS / 2)
 cmd = """Base.runtests(["LinearAlgebra"]; propagate_project=true, ncores=$ncores)"""
-run(`$(Base.julia_cmd()) --sysimage=$sysimage --project=$current_dir -e $cmd`)
+withenv("JULIA_NUM_THREADS" => 1) do
+    run(`$(Base.julia_cmd()) --sysimage=$sysimage --project=$current_dir -e $cmd`)
+end

ae41002c95704b7bf11e3f05bb4a1c359604fd00
Andy Dienes <andydienes@gmail.com>
2024-12-21 09:22:40 -0500

    don't use reducedim_initarray

--- src/tridiag.jl
+++ src/tridiag.jl
@@ -893,7 +893,13 @@ function Base._sum(A::SymTridiagonal, ::Colon)
 end
 
 function Base._sum(A::Tridiagonal, dims::Integer)
-    res = Base.reducedim_initarray(A, dims, zero(eltype(A)))
+    Base._check_valid_region(dims)
+    ax = (dims == 1) ? (1, axes(A, 2)) :
+         (dims == 2) ? (axes(A, 1), 1) :
+         axes(A)
+    res = Base.mapreduce_similar(A, eltype(A), ax)
+    fill!(res, zero(eltype(A)))
+
     n = length(A.d)
     if n == 0
         return res
@@ -927,7 +933,13 @@ function Base._sum(A::Tridiagonal, dims::Integer)
 end
 
 function Base._sum(A::SymTridiagonal, dims::Integer)
-    res = Base.reducedim_initarray(A, dims, zero(eltype(A)))
+    Base._check_valid_region(dims)
+    ax = (dims == 1) ? (1, axes(A, 2)) :
+         (dims == 2) ? (axes(A, 1), 1) :
+         axes(A)
+    res = Base.mapreduce_similar(A, eltype(A), ax)
+    fill!(res, zero(eltype(A)))
+
     n = length(A.dv)
     if n == 0
         return res
